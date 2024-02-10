2332c1103f904786263a5afd9c5ed84a7bc93940
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2024-02-10 17:45:32 +0900

    feat: auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,91 +1,72 @@
 package auth
 
 import (
-	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"os"
-
-	"golang.org/x/oauth2"
 )
 
-// TODO
-const (
-	AuthURL  = ""
-	TokenURL = ""
-)
-
-type Authenticator struct {
-	config *oauth2.Config
+type Token struct {
+	Token string `json:"token"`
 }
 
-type AuthenticatorOption func(a *Authenticator)
-
-func WithClientID(id string) AuthenticatorOption {
-	return func(a *Authenticator) {
-		a.config.ClientID = id
-	}
+type Session struct {
+	Session struct {
+		Name       string `json:"name"`
+		Key        string `json:"key"`
+		Subscriber int    `json:"subscriber"`
+	} `json:"session"`
 }
 
-func WithClientSecret(secret string) AuthenticatorOption {
-	return func(a *Authenticator) {
-		a.config.ClientSecret = secret
+func GetToken(apiKey string, res *Token) error {
+	resp, err := http.Get(fmt.Sprintf("https://ws.audioscrobbler.com/2.0/?method=auth.gettoken&api_key=%s&format=json", apiKey))
+	if err != nil {
+		return err
 	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("fetchRequestToken: %s", resp.Status)
+	}
+	if err := json.NewDecoder(resp.Body).Decode(res); err != nil {
+		return err
+	}
+	return nil
 }
 
-func WithScopes(scopes ...string) AuthenticatorOption {
-	return func(a *Authenticator) {
-		a.config.Scopes = scopes
-	}
+func AuthToken(apiKey, requestToken string) {
+	authorizationURL := fmt.Sprintf("https://www.last.fm/api/auth/?api_key=%s&token=%s", apiKey, requestToken)
+
+	fmt.Println("Please visit the following URL to authorize the application:")
+	fmt.Println(authorizationURL)
+	fmt.Println("Press Enter after authorization")
+	fmt.Scanln()
 }
 
-func WithRedirectURL(url string) AuthenticatorOption {
-	return func(a *Authenticator) {
-		a.config.RedirectURL = url
-	}
-}
+func GetSessionKey(apiKey, requestToken, secretKey string, res *Session) error {
+	methodSig := "api_key" + apiKey + "methodauth.getSession" + "token" + requestToken + secretKey
+	apiSig := getMD5Hash(methodSig)
 
-func New(opts ...AuthenticatorOption) *Authenticator {
-	cfg := &oauth2.Config{
-		ClientID:     os.Getenv("LASTFM_ID"),
-		ClientSecret: os.Getenv("LASTFM_SECRET"),
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  AuthURL,
-			TokenURL: TokenURL,
-		},
+	resp, err := http.Get(fmt.Sprintf("https://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=%s&token=%s&api_sig=%s&format=json", apiKey, requestToken, apiSig))
+	if err != nil {
+		return err
 	}
+	defer resp.Body.Close()
 
-	a := &Authenticator{
-		config: cfg,
-	}
-
-	for _, opt := range opts {
-		opt(a)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("fetchSessionKey: %s", resp.Status)
 	}
 
-	return a
+	if err := json.NewDecoder(resp.Body).Decode(res); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-var ShowDialog = oauth2.SetAuthURLParam("show_dialog", "true")
-
-func (a Authenticator) AuthURL(state string, opts ...oauth2.AuthCodeOption) string {
-	return a.config.AuthCodeURL(state, opts...)
-}
-
-func (a Authenticator) Token(ctx context.Context, state string, r *http.Request, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
-	// TODO
-
-	return a.config.Exchange(ctx, r.URL.Query().Get("code"))
-}
-
-func (a Authenticator) RefreshToken(ctx context.Context, token *oauth2.Token) (*oauth2.Token, error) {
-	src := a.config.TokenSource(ctx, token)
-	return src.Token()
-}
-
-func (a Authenticator) Exchange(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
-	return a.config.Exchange(ctx, code, opts...)
-}
-
-func (a Authenticator) Client(ctx context.Context, token *oauth2.Token) *http.Client {
-	return a.config.Client(ctx, token)
+func getMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
--- authlastfm.go
+++ authlastfm.go
@@ -1,3 +1,19 @@
 // Package authlastfm provides utilities for authenticating
 // with Last.fm's API.
 package authlastfm
+
+type Client struct {
+	baseURL   string
+	userName  string
+	apiKey    string
+	secretKey string
+}
+
+func New(userName, apiKey, secretKey string) *Client {
+	return &Client{
+		baseURL:   "https://ws.audioscrobbler.com/2.0/",
+		userName:  userName,
+		apiKey:    apiKey,
+		secretKey: secretKey,
+	}
+}
